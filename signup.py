f5e12b219c65d5df9de12faa366f6bbdf84604dd
Loice-KaniniMwau <mwauloice@gmail.com>
2023-05-15 23:10:00 +0300

    sign up enables a first time user to set up their kijani website account

---
+++ signup.py
@@ -0,0 +1,32 @@
+# the sign up class which has firstname ,lastname,phone.no,password and password
+# confirmation as well as delivery location enables first first users to set up their 
+# kijani groceries accounts.
+
+class signup:
+    def __init__(self,First_Name,Last_Name,Email_Address,Phone_No,Account_password,Password_Conf,Delivery_Location) :
+        self.First_Name=First_Name
+        self.Last_Name=Last_Name
+        self.Email_Address=Email_Address
+        self.Phone_No=Phone_No
+        self.Account_password=Account_password   
+        self.Password_Conf=Password_Conf
+        self.Delivery_Location=Delivery_Location
+# password confirmation checks if the account password is correct
+
+    def password_conf(self):
+        if(self.Account_password==self.password_conf):
+            print( f" Please add your {self.Delivery_Location}")
+        else:
+            print(f"please enter the correct password")
+    def signup_confirmation(self,firstname,lastname,password,confirmation,email,location,signup):
+         self.firstname=firstname
+         self.lastname=lastname
+         self.password=password
+         self.cofirmation=confirmation
+         self.email=email
+         self.location=location
+         self.signup=signup
+         
+         if(self.First_Name == self.firstname and self.Last_Name ==self.lastname and self.Email_Address ==self.email
+            and self.Account_password==self.password and self.Delivery_Location ==self.location):
+             self.signup_confirmation==self.signup
